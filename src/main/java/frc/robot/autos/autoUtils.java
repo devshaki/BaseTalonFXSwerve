ba17f6dd41f86f19cd3178452a3fa30df630c7e2
NufarLA <amiaznuf@gmail.com>
2024-03-15 14:23:10 +0200

    the auto utils
    auto utils for the sake of not writing the same six lines over and over

---
+++ src/main/java/frc/robot/autos/autoUtils.java
@@ -0,0 +1,45 @@
+package frc.robot.autos;
+
+import frc.robot.Constants;
+import frc.robot.autos.SubCommand.SingleNoteAuto;
+import frc.robot.autos.SubCommand.intakeNoteAuto;
+import frc.robot.subsystems.Swerve;
+
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.Intake.IntakeCommand;
+
+import frc.robot.subsystems.Arm.ArmSubsystem;
+import frc.robot.subsystems.Intake.IntakeSubsystem;
+import frc.robot.subsystems.Shooter.ShooterSubsystem;
+
+public class autoUtils {
+    public static SwerveControllerCommand CommandFromTrajectory(Trajectory trajectory, Swerve swerve, ProfiledPIDController thetaController)
+    {
+        SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(trajectory,
+        swerve::getPoseInvertedGyro,
+        Constants.Swerve.swerveKinematics,
+        new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+        new PIDController(Constants.AutoConstants.kPYController, 0, 0),
+        thetaController,
+        swerve::setModuleStates,
+        swerve);
+
+        return swerveControllerCommand;
+    }
+}
